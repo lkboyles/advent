8e92fd791d32eadefc23d53171cd22e216d36563
Tanner Hobson <thobson125@gmail.com>
2015-12-02 12:05:04 -0500

    Add code for day 2

---
+++ code/02/part_1.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+"""Solve Day 2/Part 1 of the AdventOfCode
+
+The elves are running low on wrapping paper, and so they need to
+submit an order for more. They have a list of the dimensions (length
+l, width w, and height h) of each present, and only want to order
+exactly as much as they need.
+
+Fortunately, every present is a box (a perfect right rectangular
+prism), which makes calculating the required wrapping paper for each
+gift a little easier: find the surface area of the box, which is 2*l*w
++ 2*w*h + 2*h*l. The elves also need a little extra paper for each
+present: the area of the smallest side.
+
+For example:
+
+- A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52
+  square feet of wrapping paper plus 6 square feet of slack, for a
+  total of 58 square feet.
+
+- A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42
+  square feet of wrapping paper plus 1 square foot of slack, for a
+  total of 43 square feet.
+
+All numbers in the elves' list are in feet. How many total square feet
+of wrapping paper should they order?
+
+"""
+
+def get_total_wrapping_paper_area(dimensions):
+    """Return the total area needed for all boxes
+
+    This function accepts an iterator which returns length, width,
+    height tuples and determines the total area needed for all of the
+    boxes.
+
+    >>> get_total_wrapping_paper_area([(1, 1, 1)])
+    7
+    >>> get_total_wrapping_paper_area([(1, 1, 1), (1, 1, 10)])
+    50
+    >>> get_total_wrapping_paper_area([])
+    0
+
+    """
+    total_area = 0
+    for (length, width, height) in dimensions:
+        total_area += get_wrapping_paper_area(length, width, height)
+
+    return total_area
+
+def get_wrapping_paper_area(length, width, height):
+    """Return the area of paper needed to cover the box
+
+    The area is found by taking the surface area of the box (which is
+    the sum of the areas of each side) and then adding a little bit
+    extra slack for the elves to wrap with.
+
+    The slack is defined as the area of the smallest side.
+
+    >>> get_wrapping_paper_area(1, 1, 1)
+    7
+    >>> get_wrapping_paper_area(1, 1, 10)
+    43
+    >>> get_wrapping_paper_area(2, 3, 4)
+    58
+
+    """
+    sides = (
+        length * width,
+        width * height,
+        length * height,
+    )
+
+    return 2 * sum(sides) + min(sides)
+
+def dimensions_from_line(line):
+    """Get dimensions from a single line of text
+
+    Dimensions in a file are expected to be in the form:
+    "LxWxH". Where L, W, and H are integers. For example, 2x4x8
+    represents the dimensions of a box with length 2, width 4, and
+    height 8.
+
+    >>> dimensions_from_line("1x2x3")
+    (1, 2, 3)
+    >>> dimensions_from_line("2x4x8")
+    (2, 4, 8)
+    >>> dimensions_from_line("")
+    Traceback (most recent call last):
+     ...
+    ValueError: invalid literal for int() with base 10: ''
+
+    """
+    (length, width, height) = map(int, line.split('x'))
+
+    return (length, width, height)
+
+def dimension_reader(fileobj):
+    """Return iterator for each set of dimensions in file
+
+    Dimensions are in the form specified by
+    :func:`dimensions_from_line`.
+
+    >>> list(dimension_reader(["2x4x8", "1x2x3"]))
+    [(2, 4, 8), (1, 2, 3)]
+    >>> list(dimension_reader([]))
+    []
+
+    """
+    for line in fileobj:
+        (length, width, height) = dimensions_from_line(line)
+        yield (length, width, height)
+
+def main(filename):
+    """Read dimensions from file and print the total wrapping paper area"""
+    with open(filename, 'r') as f:
+        iterator = dimension_reader(f)
+        total_area = get_total_wrapping_paper_area(iterator)
+
+        print(total_area)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    args = parser.parse_args()
+
+    main(**vars(args))
---
+++ code/02/part_2.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+"""Solve Day 2/Part 2 of the AdventOfCode
+
+The elves are also running low on ribbon. Ribbon is all the same
+width, so they only have to worry about the length they need to order,
+which they would again like to be exact.
+
+They have a list of the dimensions (length l, width w, and height h)
+of each present, and only want to order exactly as much as they
+need. Fortunately, every present is a box (a perfect right rectangular
+prism).
+
+The ribbon required to wrap a present is the shortest distance around
+its sides, or the smallest perimeter of any one face. Each present
+also requires a bow made out of ribbon as well; the feet of ribbon
+required for the perfect bow is equal to the cubic feet of volume of
+the present. Don't ask how they tie the bow, though; they'll never
+tell.
+
+For example:
+
+- A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon
+  to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for
+  a total of 34 feet.
+
+- A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon
+  to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for
+  a total of 14 feet.
+
+How many total feet of ribbon should they order?
+
+"""
+
+import part_1
+
+def get_total_ribbon(dimensions):
+    """Return the total ribbon needed for all presents
+
+
+    >>> get_total_ribbon([])
+    0
+    >>> get_total_ribbon([(1, 1, 1)])
+    5
+    >>> get_total_ribbon([(1, 1, 1), (2, 3, 4)])
+    39
+
+    """
+    total_length = 0
+    for (length, width, height) in dimensions:
+        total_length += get_total_ribbon_for_present(length, width, height)
+
+    return total_length
+
+def get_total_ribbon_for_present(length, width, height):
+    """Return the total ribbon needed for one present
+
+    The total is just the amount needed for wrapping the present plus
+    the amount for the bow.
+
+    >>> get_total_ribbon_for_present(2, 3, 4)
+    34
+    >>> get_total_ribbon_for_present(1, 1, 10)
+    14
+    >>> get_total_ribbon_for_present(1, 1, 1)
+    5
+
+    """
+    ribbon_length = get_ribbon_for_wrapping_present(length, width, height)
+    ribbon_length += get_ribbon_for_bow(length, width, height)
+
+    return ribbon_length
+
+def get_ribbon_for_wrapping_present(length, width, height):
+    """Return the amount of ribbon needed to wrap the present
+
+    This is the amount of ribbon needed to go around the smallest
+    perimeter of any face.
+
+    >>> get_ribbon_for_wrapping_present(2, 3, 4)
+    10
+    >>> get_ribbon_for_wrapping_present(1, 1, 10)
+    4
+    >>> get_ribbon_for_wrapping_present(1, 1, 1)
+    4
+
+    """
+    # To find the smallest face, we can sort the dimensions, so that
+    # the smallest two dimensions are at the beginning of the list,
+    # and then summing them up will give us half the total length, so
+    # we have to double it to get the final length.
+    dimensions = sorted([length, width, height])
+
+    ribbon_length = 2 * sum(dimensions[:2])
+
+    return ribbon_length
+
+def get_ribbon_for_bow(length, width, height):
+    """Return the amount of ribbon needed to make the bow
+
+    The bow, for some magical reason, is simply the volume of the
+    present.
+
+    >>> get_ribbon_for_bow(2, 3, 4)
+    24
+    >>> get_ribbon_for_bow(1, 1, 10)
+    10
+    >>> get_ribbon_for_bow(1, 1, 1)
+    1
+
+    """
+    ribbon_length = length * width * height
+
+    return ribbon_length
+
+def main(filename):
+    """Read dimensions from file and print the total ribbon needed"""
+    with open(filename, 'r') as f:
+        iterator = part_1.dimension_reader(f)
+        total_ribbon = get_total_ribbon(iterator)
+
+        print(total_ribbon)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    args = parser.parse_args()
+
+    main(**vars(args))
